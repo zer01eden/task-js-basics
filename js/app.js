110246b0fb232eecb5162f6e985fb9cab7bd76e6
zero_jeden <zeroitjeden@gmail.com>
2024-06-24 22:47:29 +0200

    solution 1

--- js/app.js
+++ js/app.js
@@ -1,39 +1,46 @@
 function Calculator() {
-    this.actions = ['+', '-', '*', '/', '^'];
-    this.history = [];
+	this.actions = ['+', '-', '*', '/', '^'];
+	this.history = [];
 }
 
-Calculator.prototype.isCorrectAction = function(action) {
-    return this.actions.includes(action);
-}
+Calculator.prototype.isCorrectAction = function (action) {
+	return this.actions.includes(action);
+};
 
-Calculator.prototype.getHistoryAsString = function() {
-    return this.history.join('\n');
-}
+Calculator.prototype.getHistoryAsString = function () {
+	return this.history.join('\n');
+};
 
-Calculator.prototype.add = function(num1, num2) {
-    // 1. zamień wartości przekazane przez parametr na typ number
-    // 2. sprawdź czy są one poprawne
-    // 3. jeśli tak to wykonaj działanie i zapisz jego resultat
-    // 4. dodaj do historii operacji to działanie w fomie: 1 + 1 = 2
-}
+Calculator.prototype.add = function (num1, num2) {
+	// 1. zamień wartości przekazane przez parametr na typ number
+	const number1 = parseInt(num1);
+	const number2 = parseInt(num2);
+	// 2. sprawdź czy są one poprawne
+	if ((number1, number2 === typeof number)) {
+		result = number1 + number2;
+	} else {
+		console.log('error');
+	}
+	// 3. jeśli tak to wykonaj działanie i zapisz jego resultat
+	// 4. dodaj do historii operacji to działanie w fomie: 1 + 1 = 2
+};
 
 const calc = new Calculator();
-let action, promptContent, isCorrectAction, number1, number2;
-do { 
-    promptContent = 'Podaj jaką operację chcesz wykonać (+, -, *, /, ^) i potwierdź. \n'; // \n - znak nowej linii
-    promptContent += 'Jeśli chcesz zrezygnować wciśnij Anuluj. \n';
-    promptContent += 'Lista poprzednich operacji: \n' + calc.getHistoryAsString();
+let action='+', promptContent, isCorrectAction, number1, number2;
+do {
+	promptContent =
+		'Podaj jaką operację chcesz wykonać (+, -, *, /, ^) i potwierdź. \n'; // \n - znak nowej linii
+	promptContent += 'Jeśli chcesz zrezygnować wciśnij Anuluj. \n';
+	promptContent += 'Lista poprzednich operacji: \n' + calc.getHistoryAsString();
 
-    action = prompt(promptContent);
-    isCorrectAction = calc.isCorrectAction(action);
-    if(isCorrectAction) {
-        number1 = prompt('Podaj liczbę nr 1');
-        number2 = prompt('Podaj liczbę nr 2');
+	action = prompt(promptContent);
+	isCorrectAction = calc.isCorrectAction(action);
+	if (isCorrectAction) {
+		number1 = prompt('Podaj liczbę nr 1');
+		number2 = prompt('Podaj liczbę nr 2');
 
-        if(action === '+') {
-            calc.add(number1, number2);
-        }
-    }
-    
-} while(calc.isCorrectAction(action));
+		if (action === '+') {
+			calc.add(number1, number2);
+		}
+	}
+} while (calc.isCorrectAction(action));
